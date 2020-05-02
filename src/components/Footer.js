54aa545b634f6e4547199e6b4625fdd4c14f9204
Ian Forrest <ianforrest@gmail.com>
2020-05-02 13:15:41 -0700

    logo switch to png

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/ianforrest.svg'
+import logo from '../img/ianforrest.png'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
@@ -15,7 +15,7 @@ const Footer = class extends React.Component {
           <img
             src={logo}
             alt="Ian Forrest"
-            style={{ width: '14em', height: '10em' }}
+            style={{ width: '14em'}}
           />
         </div>
         <div className="content has-text-centered has-text-white-ter">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/ianforrest.svg'
+import logo from '../img/ianforrest.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Ian Forrest"  style={{ width: '14em' }}/>
+              <img src={logo} alt="Ian Forrest"  style={{  }}/>
 
             </Link>
             {/* Hamburger menu */}
