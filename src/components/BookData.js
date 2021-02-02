5d2d3d9f8f770d4fa040c29fc1c0932869153d93
Ian Forrest <ianforrest@gmail.com>
2021-02-01 22:23:23 -0800

    design - multi columns, currently reading tag

--- src/components/BookData.js
+++ src/components/BookData.js
@@ -14,6 +14,7 @@ export const BookData = () => {
                   background 
                   status {
                       complete
+                      completeDate
                       rating
                   }
                   description
--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -17,15 +17,31 @@ const BookShelf = (props) => {
                     background: booksEdge.node.background,
                     status: {
                         complete: booksEdge.node.status.complete,
+                        completeDate: booksEdge.node.status.completeDate,
                         rating: booksEdge.node.status.rating
                     }
                 });
             });
-    return booksList.slice(0,3);
+           
+    return booksList;
   
 };
 
-const booksList = getBooksList();
+const booksList = getBooksList().sort(function(a, b) {
+    var nameA = new Date(a.status.completeDate)
+    var nameB = new Date(b.status.completeDate)
+    var nameAMilli = nameA.getTime();
+    var nameBMilli = nameB.getTime();
+
+    if (nameAMilli < nameBMilli) {
+        return -1;
+    }
+    if (nameAMilli > nameBMilli) {
+        return 1
+    }
+    return 0
+});
+
   
 return (
 
@@ -33,7 +49,7 @@ return (
         <h2>Bookshelf</h2>
         <p>Here's what I've been reading lately.</p>
         
-        <div className="columns book-shelf is-vcentered">
+        <div className="columns book-shelf is-multiline is-vcentered">
             {booksList.map((book) => (
             <div className="column is-one-third" key={book.title}>
                 <div className="box">
@@ -62,13 +78,15 @@ return (
 }
 
 function BookRating(props) {
- const bookRating = Array.apply(null, { length: props.rating }).map((e, i) => (
-    <BsStarFill />
-  ));
-
-    return (
-        bookRating
-    );
+   
+   if (props.rating) {
+      return Array.apply(null, { length: props.rating }).map((e, i) => (
+        <BsStarFill key={i} />
+    
+      ));
+   } else
+   return <span className="tag is-primary" style={{backgroundColor: "#3da9fc", textTransform: "uppercase", color: "#fffffe"}}>CURRENTLY READING</span>
+   ;
 }
 
 
